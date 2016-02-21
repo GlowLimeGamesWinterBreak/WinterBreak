1179c0cd0009f6201551f732cc6798cda0912fa5
elementarymaria <maria202577@gmail.com>
2016-02-21 12:41:59 -0500

    fuck all of you

---
+++ WinterBreak-master/Assets/Button.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour {
+
+	public void loadScene (string level){
+		Application.LoadLevel ("Untitled");
+	}
+
+	public void Update(){
+		if (Input.GetKeyDown (KeyCode.Return)) {
+			Application.LoadLevel ("Untitled");
+		}
+	}
+
+//	// Use this for initialization
+//	void Start () {
+//	
+//	}
+
+}
---
+++ WinterBreak-master/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour {
+
+	public int startingHealth = 100;
+	public int currentHealth;
+	public Slider healthSlider;
+	public int amount; // ???
+
+	bool isDead;
+	bool damaged;
+
+
+	// Use this for initialization
+	void Awake () {
+		currentHealth = startingHealth;
+		GameObject temp = GameObject.FindGameObjectWithTag ("HealthSlider");
+		if (temp != null) {
+			healthSlider = temp.GetComponent<Slider> ();
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (damaged) {
+			currentHealth -= amount;
+		}
+		damaged = false; // resets the bool???
+	
+	}
+
+	public void TakeDamage(int amount){
+		// Set the damaged flag so the screen will flash. (won't flash for now)
+		damaged = true;
+
+		// Reduce the current health by the damage amount.
+		currentHealth -= amount;
+
+		// Set the health bar's value to the current health.
+		healthSlider.value = currentHealth;
+
+//		// Play the hurt sound effect.
+//		playerAudio.Play ();
+
+		// If the player has lost all it's health and the death flag hasn't been set yet...
+		if(currentHealth <= 0 && !isDead)
+		{
+			// ... it should die.
+			Death ();
+		}
+	}
+
+	void Death ()
+	{
+		// Set the death flag so this function won't be called again.
+		isDead = true;
+
+//		// Turn off any remaining shooting effects.
+//		playerShooting.DisableEffects ();
+
+//		// Tell the animator that the player is dead.
+//		anim.SetTrigger ("Die");
+
+//		// Set the audiosource to play the death clip and play it (this will stop the hurt sound from playing).
+//		playerAudio.clip = deathClip;
+//		playerAudio.Play ();
+
+		// Turn off the movement and shooting scripts.
+//		playerMovement.enabled = false;
+//		playerShooting.enabled = false;
+	}       
+}
