1df42588930d557e1fcf228dda1e1b57c6c26b62
elementarymaria <maria202577@gmail.com>
2016-03-29 20:53:15 -0400

    PlsWork

--- WinterBreak-master/Assets/Scripts/FirstPersonController.cs
+++ WinterBreak-master/Assets/Scripts/FirstPersonController.cs
@@ -17,6 +17,12 @@ public class FirstPersonController : MonoBehaviour {
 	public Slider sprintSlider;
 	public bool damaged = false;
 
+	float TopExtent;
+	float BotExtent;
+	float Speed;
+	bool BobbingUp;
+	Vector3 Change;
+
 	PlayerHealth playerHealth;
 
 	CharacterController characterController;
@@ -26,16 +32,21 @@ public class FirstPersonController : MonoBehaviour {
 		Screen.lockCursor = true;
 		characterController = GetComponent<CharacterController> ();
 		walkSound = GetComponent<AudioSource> ();
-		GameObject temp = GameObject.FindGameObjectWithTag ("HealthSlider"); //
-		if (temp != null) { //
-			playerHealth = temp.GetComponent<PlayerHealth> (); //
-//			pizzas = new Texture[pizzaIndex];
-		} //
-		GameObject temp2 = GameObject.FindGameObjectWithTag ("SprintSlider"); //
-		if (temp2 != null) { //
-			sprintSlider = temp2.GetComponent<Slider> (); //
-//			sprintSlider.value = 0;
-		} //
+		GameObject temp = GameObject.FindGameObjectWithTag ("HealthSlider");
+		if (temp != null) {
+			playerHealth = temp.GetComponent<PlayerHealth> ();
+		}
+		GameObject temp2 = GameObject.FindGameObjectWithTag ("SprintSlider");
+		if (temp2 != null) {
+			sprintSlider = temp2.GetComponent<Slider> ();
+		}
+		//height defines how high your head bobs up IN METERS
+		TopExtent = .1f;
+		BotExtent = transform.localPosition.y;
+		// speed is in m/s
+		Speed = .1f;
+		Change = Vector3.zero;
+		BobbingUp = false;
 	}
 
 	// Update is called once per frame
@@ -51,7 +62,6 @@ public class FirstPersonController : MonoBehaviour {
 		playerCamera.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
 
 		//movement
-
 		float forwardSpeed = Input.GetAxis ("Vertical") * movementSpeed;
 		float sideSpeed = Input.GetAxis ("Horizontal") * movementSpeed;
 
@@ -64,17 +74,60 @@ public class FirstPersonController : MonoBehaviour {
 		Vector3 speed = new Vector3 (sideSpeed, verticalVelocity, forwardSpeed * movementSpeed); //x,y,z forwardspeed is forward and back
 
 		speed = transform.rotation * speed;
-		if (forwardSpeed < .01 && sideSpeed < .01)
+		if (forwardSpeed < .01 && sideSpeed < .01) {
 			walkSound.Stop ();
-		else if (walkSound.isPlaying != true)
+			// no bob
+		} 
+		else if (walkSound.isPlaying != true) {
 			walkSound.Play ();
+
+
+			// make camera bob
+//			StartCoroutine(Bobbing());
+//			temp.y += 3.0f; // modify the component you want in the variable...
+//			playerCamera.transform.position = temp; // and save the modified value
+//			temp.y -= 5.0f; // modify the component you want in the variable...
+//			playerCamera.transform.position = temp; // and save the modified value
+
+//			playerCamera.transform.localPosition.y -= 0.5f;
+//			playerCamera.transform.localPosition.y += 0.5f;
+//			verticalRotation -= movementSpeed * 0.5f;
+//			verticalRotation += movementSpeed * 0.5f;
+
+
+//			float shake = 1.0f;
+//			playerCamera.transform.localPosition = Random.insideUnitSphere * 0.7;
+//			shake -= Time.deltaTime * 1.0f;
+		}
 
 
 		//cc.SimpleMove(speed); //takes care of gravity, ignores y. jump/fly/explosion cannot use simple
 
 		characterController.Move (speed * Time.deltaTime); //does not expect speed, it expects how many units of distance traveling this frame
+//		if(forwardSpeed > .02 || sideSpeed > .02){
+//			BobbingUp = true;
+//			StartCoroutine(Bobbing());
+//			characterController.Move (speed * Time.deltaTime);
+//		}
 
+//		Change = new Vector3(0,Speed * Time.deltaTime,0);
+//
+//		if(BobbingUp)
+//		{
+//			if(transform.localPosition.y < TopExtent)
+//				transform.localPosition += Change;
+//			else
+//				BobbingUp = false;
+//		}
+//		else{
+//			if(transform.localPosition.y > BotExtent)
+//				transform.localPosition -= Change;
+//			else
+//				BobbingUp = true;
+//		}
 	}
+
+
 
 	void OnTriggerEnter (Collider col){
 		print (col.gameObject.name);
@@ -83,30 +136,6 @@ public class FirstPersonController : MonoBehaviour {
 			Destroy (col.gameObject);
 			sprintSlider.value = 10;
 			StartCoroutine(Sprinting());
-//			sptimer = Time.deltaTime; //
-
-//			while(sprintSlider.value > 0){
-//				movementSpeed = 10.0f; // was 5.0f
-//				jumpSpeed = 14.0f; // was 7.0f
-//				Update ();
-//				sprintSlider.value -= 1;
-
-////				sptimer += 1;
-//			}
-
-//			movementSpeed = 10.0f; // was 5.0f
-//			jumpSpeed = 14.0f; // was 7.0f
-
-//			if (timer <= 10.0f) {
-//				sprintSlider.value = 10.0f - timer;
-//			} //
-//			else {
-//				sprintSlider.value = 0;
-//				movementSpeed = 5.0f;
-//				jumpSpeed = 7.0f;
-//
-//			}
-
 		}
 		if (col.gameObject.name == "Enemy" || col.gameObject.name == "Enemy(Clone)") {
 //			playerHealth.TakeDamage (1);
@@ -122,6 +151,9 @@ public class FirstPersonController : MonoBehaviour {
 
 		if (col.gameObject.name == "PowerupHealth" || col.gameObject.name == "PowerupHealth(Clone)") {
 			playerHealth.currentHealth += 4;
+			if (playerHealth.currentHealth > playerHealth.startingHealth) {
+				playerHealth.currentHealth = playerHealth.startingHealth;
+			}
 			Destroy (col.gameObject);
 		}
 	}
@@ -137,4 +169,25 @@ public class FirstPersonController : MonoBehaviour {
 		movementSpeed = 5.0f;
 		jumpSpeed = 7.0f;
 	}
+
+//	IEnumerator Bobbing() {
+//		Vector3 original = playerCamera.transform.position;
+//		Vector3 temp = playerCamera.transform.position; // copy to an auxiliary variable...
+//		temp.y += 1.0f; // modify the component you want in the variable...
+//		playerCamera.transform.position = temp; // and save the modified value
+//		yield return new WaitForSeconds(0.3f);
+//		temp.y -= 1.0f; // modify the component you want in the variable...
+//		playerCamera.transform.position = temp; // and save the modified value
+//		yield return new WaitForSeconds(0.3f);
+
+//		playerCamera.transform.position = original;
+
+
+//		int sprinttime = 10;
+//		while (sprintSlider.value > 0) {
+//			sprintSlider.value -= 1;
+//		}
+//		movementSpeed = 5.0f;
+//		jumpSpeed = 7.0f;
+//	}
 }
--- WinterBreak-master/Assets/Scripts/PlayerHealth.cs
+++ WinterBreak-master/Assets/Scripts/PlayerHealth.cs
@@ -4,88 +4,19 @@ using System.Collections;
 
 public class PlayerHealth : MonoBehaviour {
 
-	public Sprite[] PizzaSprites;
-	public Image Pizza;
-
 // FROM HERE
 	public int startingHealth = 12;
-	public int currentHealth;
-	public int maxHealth;
-//	public int healthPerPizza;
+	public int currentHealth = 12;
 
-//	public int amount; //
-//	public GUITexture pizzaGUI;
-	public Texture[] pizzas;
-	public Texture[] images = new Texture[13];
+	public Texture[] images = new Texture[4];
 
 	bool isDead;
 	bool damaged;
 
-//	private ArrayList pizzas = new ArrayList ();
-//	public int maxPizzasPerRow;
-//	private float spacingX;
-//	private float spacingY;
-//
 //	// Use this for initialization
 	void Awake () {
 		currentHealth = startingHealth;
-
-//		spacingX = pizzaGUI.pixelInset.width;
-//		spacingY = -pizzaGUI.pixelInset.height;
-//		AddPizzas (startingHealth / healthPerPizza);
-
-
-//		GameObject temp = GameObject.FindGameObjectWithTag ("HealthSlider");
-//		if (temp != null) {
-//			pizzas = new Texture[currentHealth+1];
-
-//			healthSlider = temp.GetComponent<Slider> ();
-
-//		}
-//	
 	}
-//	
-//	public void AddPizzas(int n){
-//		for (int i = 0; i < n; i++) {
-//			Transform newPizza = ((GameObject)Instantiate (pizzaGUI.gameObject, transform.position, Quaternion.identity)).transform;
-//			newPizza.parent = this.transform.parent;
-//
-//			int y = Mathf.FloorToInt (pizzas.Count / maxPizzasPerRow);
-//			int x = pizzas.Count - y * maxPizzasPerRow;
-//
-//			newPizza.GetComponent<GUITexture> ().pixelInset = new Rect (x * spacingX, y * spacingY, 100, 100);
-//			pizzas.Add (newPizza);
-//		}
-//		maxHealth += n * healthPerPizza;
-//		currentHealth = maxHealth;
-//		Update ();
-//
-//	}
-//
-//	public void ModifyHealth(int amount){
-//		currentHealth += amount;
-//		currentHealth = Mathf.Clamp (currentHealth, 0, maxHealth);
-//		Update ();
-//	}
-
-//	public void UpdatePizzas(){
-//		bool restAreEmpty = false;
-//		int iii = 1;
-//
-//		foreach (Transform pizza in pizzas) {
-//			if (restAreEmpty) {
-//				pizza.guiTexture = images [0];
-//			} 
-//			else {
-//				int currentPizzaHealth = (int)(healthPerPizza - (healthPerPizza * i - currentHealth));
-//				int healthPerImage = healthPerPizza / images.Length;
-//				int imageIndex;
-//
-//				pizza.guiTexture = images[imageIndex];
-//				restAreEmpty = true;
-//			}
-//		}
-//	}
 
 //	// Update is called once per frame
 	void Update () {
@@ -105,7 +36,19 @@ public class PlayerHealth : MonoBehaviour {
 	}
 
 	void OnGUI(){
-		GUI.DrawTexture (new Rect (20, 100, 150, 400), images[currentHealth], ScaleMode.ScaleToFit);
+		int pizzaAmount = currentHealth;
+		int pizzaDiv = pizzaAmount / 4;
+		int pizzaMod = (pizzaAmount % 4);
+
+		int x = -50;
+		int y = 240;
+		for (int i = 0; i < pizzaDiv; i++) {
+			GUI.DrawTexture (new Rect (x, y, 120, 120), images [4], ScaleMode.ScaleToFit);
+			x += 40;
+		}
+		if (pizzaMod > 0) {
+			GUI.DrawTexture (new Rect (x, y, 120, 120), images [pizzaMod], ScaleMode.ScaleToFit);
+		}
 	}
 
 	public void TakeDamage(int amount){
